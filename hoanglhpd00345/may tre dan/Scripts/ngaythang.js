c059205b3a9f9deeb980ded93cce5dbc699bbffa
hoanglhpd00345 <hoanglhpd00345@gmail.com>
2015-06-16 22:20:22 +0700

    Signed-off-by: hoanglhpd00345 <hoanglhpd00345@gmail.com> ass

---
+++ hoanglhpd00345/may tre dan/Scripts/ngaythang.js
@@ -0,0 +1,10 @@
+﻿var day_of_week =new Array("Chủ Nhật","Thứ Hai","Thứ Ba","Thứ Tư","Thứ Năm","Thứ Sáu","Thứ Bảy");
+var month_of_year =new Array("1","2","3","4","5","6","7","8","9","10","11","12");  
+var nowdate = new Date();   
+var curyear= nowdate.getYear();     
+var curmonth = nowdate.getMonth();  
+var curdate = nowdate.getDate();   
+var curday = nowdate.getDay();
+if (curyear < 1000) {curyear= 1900  +curyear;}
+if (curdate<10) {curdate="0"+curdate;}  
+document.write("<font size='3' color='#ffffff'>"+day_of_week[curday]+ ", "+"Ngày " + curdate + " "+"Tháng " +month_of_year[curmonth]+ " "+"Năm" + " "+curyear+"</font>");  
---
+++ hoanglhpd00345/may tre dan/Scripts/timkiem.js
@@ -0,0 +1,106 @@
+function TimKiem(){
+	
+//Giá dưới 100.000
+		if((document.form1.select1.value =='V1') &&(document.form1.select2.value == 'BG'))			
+			("../html/Timkiem0.html");
+			
+		if((document.form1.select1.value =='V1') &&(document.form1.select2.value == 'BL'))
+			window.open("../html/Timkiem0.html");
+			
+		if((document.form1.select1.value =='V1') &&(document.form1.select2.value == "TT"))
+			window.open("../html/Timkiem3_1.html");
+			
+		if((document.form1.select1.value =='V1') &&(document.form1.select2.value == 'TG'))
+			window.open("../html/Timkiem4_1.html");
+			
+		if((document.form1.select1.value =='V1') &&(document.form1.select2.value == 'KH'))
+			window.open("../html/Timkiem5_1.html");
+
+	
+//Giá từ 100k-200k
+	if((document.form1.select1.value =='V2') &&(document.form1.select2.value == 'BG'))
+			window.open("../html/Timkiem0.html");
+			
+		if((document.form1.select1.value =='V2') &&(document.form1.select2.value == 'BL'))
+			window.open("../html/Timkiem2_2.html");
+			
+		if((document.form1.select1.value =='V2') &&(document.form1.select2.value == "TT"))
+			window.open("../html/Timkiem3_2.html");
+			
+		if((document.form1.select1.value =='V2') &&(document.form1.select2.value == 'TG'))
+			window.open("../html/Timkiem4_2.html");
+			
+		if((document.form1.select1.value =='V2') &&(document.form1.select2.value == 'KH'))
+			window.open("../html/Timkiem5_2.html");
+
+			
+//Giá từ 200k-500k
+		if((document.form1.select1.value =='V3') &&(document.form1.select2.value == 'BG'))
+			window.open("../html/Timkiem0.html");
+			
+		if((document.form1.select1.value =='V3') &&(document.form1.select2.value == 'BL'))
+			window.open("../html/Timkiem2_3.html");
+			
+		if((document.form1.select1.value =='V3') &&(document.form1.select2.value == "TT"))
+			window.open("../html/Timkiem3_3.html");
+			
+		if((document.form1.select1.value =='V3') &&(document.form1.select2.value == 'TG'))
+			window.open("../html/Timkiem4_3.html");
+			
+		if((document.form1.select1.value =='V3') &&(document.form1.select2.value == 'KH'))
+			window.open("../html/Timkiem5_3.html");
+
+			
+//Giá từ 500k-1tr
+		if((document.form1.select1.value =='V4') &&(document.form1.select2.value == 'BG'))
+			window.open("../html/Timkiem0.html");
+			
+		if((document.form1.select1.value =='V4') &&(document.form1.select2.value == 'BL'))
+			window.open("../html/Timkiem2_4.html");
+			
+		if((document.form1.select1.value =='V4') &&(document.form1.select2.value == "TT"))
+			window.open("../html/Timkiem3_4.html");
+			
+		if((document.form1.select1.value =='V4') &&(document.form1.select2.value == 'TG'))
+			window.open("../html/Timkiem0.html");
+			
+		if((document.form1.select1.value =='V4') &&(document.form1.select2.value == 'KH'))
+			window.open("../html/Timkiem0.html");
+
+			
+//Giá từ 1tr-5tr
+	if((document.form1.select1.value =='V5') &&(document.form1.select2.value == 'BG'))
+			window.open("../html/Timkiem1_5.html");
+			
+		if((document.form1.select1.value =='V5') &&(document.form1.select2.value == 'BL'))
+			window.open("../html/Timkiem2_5.html");
+			
+		if((document.form1.select1.value =='V5') &&(document.form1.select2.value == "TT"))
+			window.open("../html/Timkiem0.html");
+			
+		if((document.form1.select1.value =='V5') &&(document.form1.select2.value == 'TG'))
+			window.open("../html/Timkiem0.html");
+			
+		if((document.form1.select1.value =='V5') &&(document.form1.select2.value == 'KH'))
+			window.open("../html/Timkiem0.html");
+			
+			
+//Giá trên 5tr
+		if((document.form1.select1.value =='V6') &&(document.form1.select2.value == 'BG'))
+			window.open("../html/Timkiem1_6.html");
+			
+		if((document.form1.select1.value =='V6') &&(document.form1.select2.value == 'BL'))
+			window.open("../html/Timkiem0.html");
+			
+		if((document.form1.select1.value =='V6') &&(document.form1.select2.value == "TT"))
+			window.open("../html/Timkiem0.html");
+			
+		if((document.form1.select1.value =='V6') &&(document.form1.select2.value == 'TG'))
+			window.open("../html/Timkiem0.html");
+			
+		if((document.form1.select1.value =='V6') &&(document.form1.select2.value == 'KH'))
+			window.open("../html/Timkiem0.html");
+			
+		
+	}
+// JavaScript Document
